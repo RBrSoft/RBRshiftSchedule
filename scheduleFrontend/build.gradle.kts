ab842c0150368fee21460ce1b4928aa2fd621a04
Roman Breuer <roman.breuer@rbrsoft.de>
2021-05-20 00:36:00 +0200

    Start Frontend

--- scheduleFrontend/build.gradle.kts
+++ scheduleFrontend/build.gradle.kts
@@ -1,15 +1,16 @@
 
 plugins {
-  id("com.moowork.node") version "1.2.0"
+  id("com.moowork.node") version "1.3.1"
 }
 
 group = "de.rbrsoft"
 version = "0.0.1"
 
 node {
-  version = "16.1.0"
-  npmVersion = "6.14.13"
-  download = false
+  setProperty("version", "16.1.0")
+  setProperty("npmVersion", "6.14.13")
+  setProperty("download", false)
+
   workDir = file("${project.buildDir}/node")
   nodeModulesDir = file("${project.projectDir}")
 }
