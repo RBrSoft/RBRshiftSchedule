3a8f7ca9189f6f4867425e6770c2f8952d14ae9d
Roman Breuer <roman.breuer@rbrsoft.de>
2021-06-17 18:46:23 +0200

    Implement styling

--- scheduleFrontend/src/app/in-memory-data.service.ts
+++ scheduleFrontend/src/app/in-memory-data.service.ts
@@ -11,13 +11,23 @@ export class InMemoryDataService implements InMemoryDbService {
 
   createDb() {
     const teams = [
-      {id: "abc", teamName: "Die Schnullies"},
-      {id: "cde", teamName: "Team1715"}
+      {id: 'abc', teamName: 'Die Schnullies'},
+      {id: 'cde', teamName: 'Team1715'},
+      {id: 'efg', teamName: 'Alle meine Entchen'},
+      {id: 'ghi', teamName: 'Santa\'s Six'},
+      {id: 'ijk', teamName: 'Oceans Eleven'}
     ];
 
     const members = [
-      {id: "1234", nickname: "Kunibert"},
-      {id: "4567", nickname: "Wilhelm"},
+      {id: '1', nickname: 'Kunibert'},
+      {id: '2', nickname: 'Wilhelm'},
+      {id: '3', nickname: 'Sir Stone'},
+      {id: '4', nickname: 'Alfonso'},
+      {id: '5', nickname: 'Bruce Wayne'},
+      {id: '6', nickname: 'Mrs. T'},
+      {id: '7', nickname: 'OhneRotkohl'},
+      {id: '8', nickname: 'Lustiges Lieschen'},
+      {id: '9', nickname: 'Bob'}
     ];
 
     return {teams, members};
