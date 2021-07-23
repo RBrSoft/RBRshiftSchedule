1878c0dda4acb2e54b80487742d9126510e611e3
Roman Breuer <roman.breuer@rbrsoft.de>
2021-07-23 21:28:15 +0200

    Unit Tests

--- scheduleFrontend/src/app/member/member.component.spec.ts
+++ scheduleFrontend/src/app/member/member.component.spec.ts
@@ -1,14 +1,25 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {MemberComponent} from './member.component';
+import {RouterTestingModule} from '@angular/router/testing';
+import {MemberService} from '../member.service';
+import {of} from 'rxjs/internal/observable/of';
 
 describe('MemberComponent', () => {
   let component: MemberComponent;
   let fixture: ComponentFixture<MemberComponent>;
 
   beforeEach(async () => {
+    const mockMemberService = {
+       getMembers: function () {
+         return of();
+      }
+    };
     await TestBed.configureTestingModule({
-      declarations: [ MemberComponent ]
+      declarations: [ MemberComponent ],
+      imports: [ RouterTestingModule ],
+      providers: [
+        {provide: MemberService, useValue: mockMemberService} ]
     })
     .compileComponents();
   });
--- scheduleFrontend/src/app/team/team.component.spec.ts
+++ scheduleFrontend/src/app/team/team.component.spec.ts
@@ -1,14 +1,27 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {TeamComponent} from './team.component';
+import {TeamService} from '../team.service';
+import {of} from 'rxjs/internal/observable/of';
 
 describe('TeamComponent', () => {
   let component: TeamComponent;
   let fixture: ComponentFixture<TeamComponent>;
+  let compiled;
 
   beforeEach(async () => {
+    const mockTeamService = {
+      getTeamForCurrentUser: function() {
+        return of(
+          [1, 2, 3]
+        );
+      }
+    };
     await TestBed.configureTestingModule({
-      declarations: [ TeamComponent ]
+      declarations: [ TeamComponent ],
+      providers: [
+        {provide: TeamService, useValue: mockTeamService}
+      ]
     })
     .compileComponents();
   });
@@ -17,9 +30,20 @@ describe('TeamComponent', () => {
     fixture = TestBed.createComponent(TeamComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    compiled = fixture.debugElement.nativeElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have the heading "Deine Teams"', () => {
+    expect(compiled.querySelector('h2').textContent).toContain('Deine Teams:');
+  });
+
+  it('should render one team card per team', () => {
+    const listItems = compiled.querySelectorAll('.card-list li');
+    expect(listItems.length).toEqual(3);
+  });
+
 });
