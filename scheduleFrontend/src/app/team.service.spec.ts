82b89aa1e7e906e6856b67b9aee4786f7dc4fc74
Roman Breuer <roman.breuer@rbrsoft.de>
2021-07-24 19:43:41 +0200

    Write test for team service

--- scheduleFrontend/src/app/team.service.spec.ts
+++ scheduleFrontend/src/app/team.service.spec.ts
@@ -2,14 +2,27 @@ import {TestBed} from '@angular/core/testing';
 
 import {TeamService} from './team.service';
 import {HttpClient} from '@angular/common/http';
+import {of} from 'rxjs/internal/observable/of';
+import {Team} from './team';
 
 describe('TeamService', () => {
+  const teamTestData = [
+    new Team('123', 'Team Name 1'),
+    new Team('456', 'Team Name 2')
+  ];
+  let mockHttpClient: HttpClient;
   let service: TeamService;
+
+  mockHttpClient = {
+    get: function() {
+      return of(teamTestData);
+    }
+  } as any;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
-        {provide: HttpClient, useValue: {}}
+        {provide: HttpClient, useValue: mockHttpClient}
       ]
     });
     service = TestBed.inject(TeamService);
@@ -18,4 +31,29 @@ describe('TeamService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  describe('#getTeamForCurrentUser', () => {
+
+    it('should return an Observable<Team[]>', (done) => {
+      // when
+      const result = service.getTeamForCurrentUser();
+      // then
+      result.subscribe(teams => {
+          expect(teams.length).toEqual(teamTestData.length);
+          done();
+      });
+    });
+
+    it('should call GET api/teams', (done) => {
+      // given
+      spyOn(mockHttpClient, 'get').and.callThrough();
+      // when
+      service.getTeamForCurrentUser().subscribe(() => {});
+      // then
+      expect(mockHttpClient.get).toHaveBeenCalledWith('api/teams');
+      done();
+    });
+
+  });
+
 });
