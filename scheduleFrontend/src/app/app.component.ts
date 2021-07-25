0563e263cfa6a1b6bd6b0be902a1bbdebed54773
Roman Breuer <roman.breuer@rbrsoft.de>
2021-07-25 19:29:38 +0200

    Funktionierendes ShiftShedule mit Login

--- scheduleFrontend/src/app/app.component.ts
+++ scheduleFrontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {FacebookLoginProvider, GoogleLoginProvider, SocialAuthService, SocialUser} from 'angularx-social-login';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,37 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Shift Schedule';
+  user: SocialUser;
+  loggedIn: boolean;
+
+  constructor(private authService: SocialAuthService) {
+  }
+
+  socialSignIn(provider: string): void {
+    let socialPlatform;
+    if (provider === 'google') {
+      socialPlatform = GoogleLoginProvider.PROVIDER_ID;
+    } else if (provider === 'facebook') {
+      socialPlatform = FacebookLoginProvider.PROVIDER_ID;
+    } else {
+      socialPlatform = null;
+    }
+
+    this.authService.signIn(socialPlatform).then(
+      (userData) => {
+        console.log(userData);
+        this.user = userData;
+        this.loggedIn = (userData != null);
+      }
+    );
+  }
+
+  socialSignOut(): void {
+    this.authService.signOut().then(
+      () => {
+        this.user = null;
+        this.loggedIn = false;
+      }
+    );
+  }
 }
--- scheduleFrontend/src/app/app.module.ts
+++ scheduleFrontend/src/app/app.module.ts
@@ -9,6 +9,8 @@ import {InMemoryDataService} from './in-memory-data.service';
 import {MemberComponent} from './member/member.component';
 import {AppRoutingModule} from './app-routing.module';
 
+import {FacebookLoginProvider, GoogleLoginProvider, SocialAuthServiceConfig, SocialLoginModule} from 'angularx-social-login';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,13 +20,35 @@ import {AppRoutingModule} from './app-routing.module';
   imports: [
     BrowserModule,
     HttpClientModule,
+    SocialLoginModule,
     // Um das Backend zu emulieren
     InMemoryWebApiModule.forRoot(
       InMemoryDataService, {dataEncapsulation: false}
     ),
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider(
+              '270938385499-f5jbqppbcn2r7m2sfmpsqaogr8a3sn9m.apps.googleusercontent.com'
+            )
+          },
+          {
+            id: FacebookLoginProvider.PROVIDER_ID,
+            provider: new FacebookLoginProvider(
+              'clientId'
+            )
+          }
+        ]
+      } as SocialAuthServiceConfig,
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
